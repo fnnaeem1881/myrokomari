2b9e82e42e1bcbd06e45989fadbd2cbc163071a4
fnnaeem1881 <mehedinaeem66@gmail.com>
2021-03-27 02:34:18 +0600

    Header Design Not responsive

---
+++ assetc/js/costom.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {myFunction()};
+
+var wrappy = document.getElementById("wrappy");
+
+var sticky = wrappy.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    wrappy.classList.add("sticky")
+  } else {
+    wrappy.classList.remove("sticky");
+  }
+}
+
